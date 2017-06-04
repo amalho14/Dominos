f89961d1565db67916e6d03a6cc6acd292bb018f
anveshmalhotra <anvesh.malhotra@asu.edu>
2017-06-04 04:21:52 -0700

    Continuous Integration Setup

--- src/common/test_unit_delivery_home.py
+++ src/common/test_unit_delivery_home.py
@@ -10,5 +10,11 @@ class Deliver(unittest.TestCase):
     def test_1(self):
         button_text=Keywords.getText(self.driver, "//*[contains(@class,'js-delivery')][contains(text(),'Delivery')]")
         assert (button_text=='DELIVERY'),"Text of Delivery Button is incorrect, expected: %s"%'DELIVERY'
+    def test_2(self):
+        button_text=Keywords.getText(self.driver, "//*[contains(@class,'js-delivery')][contains(text(),'Delivery')]")
+        assert (button_text=='DELIVERY'),"Text of Delivery Button is incorrect, expected: %s"%'DELIVERY'
+    def test_3(self):
+        button_text=Keywords.getText(self.driver, "//*[contains(@class,'js-delivery')][contains(text(),'Delivery')]")
+        assert (button_text=='DELIVERY'),"Text of Delivery Button is incorrect, expected: %s"%'DELIVERY'
     def tearDown(self):
         self.driver.quit()
