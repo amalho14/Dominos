cc1f3595cd4468e1abb93fd7ffcdbaab31f6840a
anveshmalhotra <anvesh.malhotra@asu.edu>
2017-06-04 08:45:31 -0700

    Continuous Integration Setup

--- coftest.py
+++ coftest.py
@@ -8,14 +8,24 @@ from selenium.webdriver.remote.remote_connection import RemoteConnection
 browsers = [
     {
         "platform": "Windows 10",
-        "browserName": "chrome",
-        "version": "58.0",
-        #"idleTimeout":"10"
+        "browserName": "MicrosoftEdge",
+        "version": "14.14393"
     }, {
-        "platform": "macOS 10.12",
+        "platform": "Windows 10",
+        "browserName": "firefox",
+        "version": "49.0"
+    }, {
+        "platform": "Windows 7",
+        "browserName": "internet explorer",
+        "version": "11.0"
+    }, {
+        "platform": "OS X 10.11",
         "browserName": "safari",
-        "version": "10.0",
-        #"idleTimeout":"10"
+        "version": "10.0"
+    }, {
+        "platform": "OS X 10.11",
+        "browserName": "chrome",
+        "version": "54.0"
     }]
 
 def pytest_generate_tests(metafunc):
