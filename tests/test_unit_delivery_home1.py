c385a103e614df6eb90f2e33f3e698196f873049
anveshmalhotra <anvesh.malhotra@asu.edu>
2017-06-04 09:20:09 -0700

    Continuous Integration Setup

--- tests/test_unit_delivery_home1.py
+++ tests/test_unit_delivery_home1.py
@@ -1,4 +1,4 @@
-from tests import Keywords
+delivery_buttonfrom tests import Keywords
 import pytest
 from tests.Setup import navigate_to_dominos
 
@@ -8,7 +8,8 @@ class TestDeliverHome(object):
     
     def test_link(self,driver):
         navigate_to_dominos(driver)
-        button_text=Keywords.getText(driver, "//*[contains(@class,'js-delivery')][contains(text(),'Delivery')]")
+        delivery_button="//*[contains(@class,'js-delivery')][contains(text(),'Delivery')]"
+        button_text=Keywords.getText(driver, delivery_button)
         assert (button_text=='DELIVERY'),"Text of Delivery Button is incorrect, expected: %s"%'DELIVERY'
-        Keywords.ClickElement(driver, button_text)
+        Keywords.ClickElement(driver, delivery_button)
         assert (driver.title=="Location Search - Location Search"),"After Delivery Button is clicked either page not visible or wrong page"
