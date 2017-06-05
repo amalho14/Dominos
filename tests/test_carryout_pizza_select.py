124389348ba786a4d9db41381f1a2035f6a94551
anveshmalhotra <anvesh.malhotra@asu.edu>
2017-06-04 22:10:24 -0700

    Select Pizza Test Modify

--- tests/test_carryout_pizza_select.py
+++ tests/test_carryout_pizza_select.py
@@ -7,18 +7,31 @@ from tests.Setup import navigate_to_dominos
 @pytest.mark.usefixtures('driver')
 class TestCarryoutSelectPizza(object):
     
-    def test_carryout_pizza_selec_unit(self,driver):
+    def test_carryout_pizza_select_unit(self,driver):
         #Get to the pizza select page
         navigate_to_dominos(driver)
         carryout_button="//*[contains(@class,'js-carryout')]"
         Keywords.ClickElement(driver, carryout_button)
         city="//*[@name='City']" 
-        Keywords.enterText(driver,"Tempe",city)
+        cityValue="TEMPE"
+        Keywords.enterText(driver,cityValue,city)
         state="//*[@name='Region']"
         Keywords.ClickElement(driver,state)
         state_AZ="//*[@name='Region']/option[contains(text(),'AZ')]"
+        stateValue="AZ"
         Keywords.ClickElement(driver,state_AZ)
         zip_code="//*[@name='Postal_Code']"
+        zip_codeValue="85281"
         Keywords.enterText(driver,"85281",zip_code)
-        driver.submit()
+        submit="//*[@type='submit']"
+        Keywords.ClickElement(driver,submit)
+        
+        myAddress="%s, %s, %s",(cityValue,state_AZ,zip_code)
+        myLocation="//*[contains(@class,'qa-MyLoc')]/li"
+        elements=Keywords.WebElements(driver,myLocation)
+        for element in elements:
+            if(element.is_empty()==False):
+                assert (element.get_text()==myAddress),"Address not same for checkout"
+                
+        
         
